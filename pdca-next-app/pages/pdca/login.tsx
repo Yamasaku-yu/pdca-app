154fad2c58f6fadc633273a9d8baf0868f2c20ac
Yamasaku-yu <yamazkaiyusaku0731@yahoo.co.jp>
2025-05-07 09:59:19 +0900

    u

--- pdca-next-app/pages/pdca/login.tsx
+++ pdca-next-app/pages/pdca/login.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { useRouter } from "next/router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import UserForm from "../components/userForm";
@@ -7,41 +7,54 @@ import Navbar from "../components/navbar";
 export default function Login() {
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
   const router = useRouter();
 
   const loginUser = async () => {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_BASE}/api/pdca/login`,
-      {
+    setLoading(true);
+
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE}/api/pdca/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         credentials: "include",
         body: JSON.stringify({ username, password }),
-      }
-    );
+      });
 
-    const data = await response.json();
-    if (response.ok) {
-      router.push(`/pdca/user/${data.userId}`);
-    } else {
-      alert(data.message);
+      const data = await response.json();
+
+      if (response.ok) {
+        router.push(`/pdca/user/${data.userId}`);
+      } else {
+        alert(data.message);
+      }
+    } catch (error) {
+      alert("ログイン時にエラーが発生しました");
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
   };
+
 
   return (
     <div>
       <Navbar brandUrl={`/pdca`}>
-        
+
       </Navbar>
-      <UserForm
-        title={"ログイン"}
-        username={username}
-        password={password}
-        setPassword={setPassword}
-        setUsername={setUsername}
-        registerUser={loginUser}
-        btnName={"ログイン"}
-      />
+      {loading ? (
+        <div>
+          <h1>ログイン中...</h1>
+        </div>) : (
+        <UserForm
+          title={"ログイン"}
+          username={username}
+          password={password}
+          setPassword={setPassword}
+          setUsername={setUsername}
+          registerUser={loginUser}
+          btnName={"ログイン"}
+        />)}
       <footer className="bg-dark text-white text-center fixed-bottom">
         画面の一番下のフッター
       </footer>
--- pdca-next-app/pages/pdca/register.tsx
+++ pdca-next-app/pages/pdca/register.tsx
@@ -7,33 +7,47 @@ import Navbar from "../components/navbar";
 export default function Register() {
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
   const router = useRouter();
 
   const registerUser = async () => {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_BASE}/api/pdca/register`,
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        credentials: "include",
-        body: JSON.stringify({ username, password }),
-      }
-    );
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_BASE}/api/pdca/register`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          credentials: "include",
+          body: JSON.stringify({ username, password }),
+        }
+      );
 
-    const data = await response.json();
-    if (response.ok) {
-      router.push(`/pdca/user/${data.userId}`);
-    } else {
-      alert(data.message);
+      const data = await response.json();
+      if (response.ok) {
+        router.push(`/pdca/user/${data.userId}`);
+      } else {
+        alert(data.message);
+      }
     }
+    catch (error) {
+      alert("ログイン時にエラーが発生しました");
+      console.error(error);
+    }
+    finally {
+      setLoading(false);
+    }
+
   };
 
   return (
     <div>
       <Navbar brandUrl={`/pdca`}>
-        
+
       </Navbar>
-      <UserForm
+      {loading ? (<div>
+        <h1>登録中...</h1>
+      </div>) : (<UserForm
         title={"ユーザー登録"}
         username={username}
         password={password}
@@ -41,7 +55,8 @@ export default function Register() {
         setUsername={setUsername}
         registerUser={registerUser}
         btnName={"登録"}
-      />
+      />)}
+
       <footer className="bg-dark text-white text-center fixed-bottom">
         画面の一番下のフッター
       </footer>
